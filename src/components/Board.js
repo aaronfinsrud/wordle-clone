1132f1dfdc0eeb5bca957975ba3b0eea8f59256e
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-04-29 00:18:54 -0400

    add checkGuess fn to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,10 +4,15 @@ import Row from './Row';
 
 function Board() {
   const NUMBER_OF_GUESSES = 6;
-  const rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
-  let [guessesRemaining, setGuessesRemaining] = useState(NUMBER_OF_GUESSES);
-  let [currentGuess, setCurrentGuess] = useState([]);
-  let [nextLetter, setNextLetter] = useState(0);
+  const [guessesRemaining, setGuessesRemaining] = useState(NUMBER_OF_GUESSES);
+  const [currentGuess, setCurrentGuess] = useState([]);
+  const [nextLetter, setNextLetter] = useState(0);
+  const [rightGuessString, setRightGuessString] = useState('');
+
+
+  useEffect(() => {
+    setRightGuessString(WORDS[Math.floor(Math.random() * WORDS.length)]);
+  }, [])
 
   useEffect(() => {
     function handleKeyboardInput(e) {
@@ -34,8 +39,8 @@ function Board() {
   function insertLetter (pressedKey) {
     if (nextLetter === 5) return;
     pressedKey = pressedKey.toLowerCase();
-    let row = document.getElementsByClassName('letter-row')[NUMBER_OF_GUESSES - guessesRemaining];
-    let box = row.children[nextLetter];
+    const row = document.getElementsByClassName('letter-row')[NUMBER_OF_GUESSES - guessesRemaining];
+    const box = row.children[nextLetter];
     box.textContent = pressedKey;
     box.classList.add('filled-box');
     setCurrentGuess([...currentGuess, pressedKey]);
@@ -52,7 +57,51 @@ function Board() {
   }
 
   function checkGuess() {
+    const row = document.getElementsByClassName('letter-row')[NUMBER_OF_GUESSES - guessesRemaining];
+    const guessString = currentGuess.join('');
+    const rightGuess = Array.from(rightGuessString);
+    if (guessString.length !== 5) {
+      alert('Not enough letters!');
+      return;
+    } else if (!WORDS.includes(guessString)) {
+      alert("Word not in list!");
+      return;
+    }
+    for (var i = 0; i < currentGuess.length; i++) {
+      const box = row.children[i];
+      const letter = currentGuess[i];
+      const letterPosition = rightGuess.indexOf(currentGuess[i]);
+      let letterColor = '';
+      if (letterPosition === -1) letterColor = 'grey';
+      else {
+        if (currentGuess[i] === rightGuess[i]) letterColor = 'green';
+        else letterColor = 'yellow';
+      }
+      rightGuess[letterPosition] = '#';
 
+      const delay = 250 * i;
+      setTimeout(()=>{
+        box.style.backgroundColor = letterColor;
+        //shadeKeyBoard(letter, letterColor);
+      }, delay)
+    }
+    if (guessString === rightGuessString) {
+      alert("You guessed right!");
+      setGuessesRemaining(0);
+      return;
+    } else {
+      setGuessesRemaining(guessesRemaining-1);
+      setCurrentGuess([]);
+      setNextLetter(0);
+      if (guessesRemaining === 0) {
+        alert(`You've run out of guesses! The correct word was: ${rightGuessString}`)
+      }
+    }
+
+  }
+
+  function shadeKeyBoard(letter, color) {
+   //TODO
   }
 
 
