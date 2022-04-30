f98b04d13ba0392baac40d823a6c8c37cd8d70e2
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-04-30 00:06:36 -0400

    begin implementing input boxes

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,7 +1,25 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-function Box() {
-  return <div className="letter-box" />
+function Box({ id, active }) {
+  return (
+    <div className="letter-box">
+      {active ? (
+        <input
+          onCompositionStart={() => () => {}}
+          onCompositionEnd={() => {}}
+          type="text"
+          name={id}
+          maxLength="1"
+        />
+      ) : null}
+    </div>
+  )
+}
+
+Box.propTypes = {
+  id: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
 }
 
 export default Box
