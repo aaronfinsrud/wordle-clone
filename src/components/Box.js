4c07f55a9c041fe67f70b4fa75efcd18af084c43
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-05-01 22:26:31 -0400

    add event handler fns

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,25 +1,67 @@
-import React from "react"
+import React, { useContext, useEffect, useRef } from "react"
 import PropTypes from "prop-types"
+import { BoardContext } from "../contexts/BoardContext"
 
-function Box({ id, active }) {
+function Box({ cell }) {
+  const { dispatch, submitWord, state } = useContext(BoardContext)
+  const inputRef = useRef(null)
+  const { guessesRemaining, nextLetter, numberOfGuesses } = state
+  const { backgroundColor, id, character } = cell
+  const [ownRow, ownCol] = id.split(".").map((str) => Number(str))
+  const activeRow = numberOfGuesses - guessesRemaining
+  const activeCol = nextLetter
+  const isActive = activeRow === ownRow && activeCol === ownCol
+
+  useEffect(() => {
+    if (isActive) {
+      inputRef.current.focus()
+    }
+  }, [isActive])
+
+  function handleInput(e) {
+    const { value } = e.target
+    if (!value) return
+    const queue = value.split("")
+    while (queue.length > 0) {
+      const action = { type: "INSERT_LETTER", character: queue.shift() }
+      dispatch(action)
+    }
+  }
+  function handleKeyDown(e) {
+    const { keyCode, target } = e
+    if (keyCode === 8 && target.value === "") {
+      const action = { type: "DELETE_LETTER" }
+      dispatch(action)
+    }
+    if (keyCode === 13) {
+      submitWord()
+    }
+  }
+
   return (
-    <div className="letter-box">
-      {active ? (
+    <div style={{ backgroundColor }} className="letter-box">
+      {isActive ? (
         <input
-          onCompositionStart={() => () => {}}
-          onCompositionEnd={() => {}}
+          onCompositionEnd={handleInput}
+          onKeyDown={handleKeyDown}
           type="text"
           name={id}
-          maxLength="1"
+          maxLength={state.rightGuessString.length - ownCol}
+          ref={inputRef}
         />
-      ) : null}
+      ) : (
+        character
+      )}
     </div>
   )
 }
 
 Box.propTypes = {
-  id: PropTypes.string.isRequired,
-  active: PropTypes.bool.isRequired,
+  cell: PropTypes.shape({
+    character: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default Box
