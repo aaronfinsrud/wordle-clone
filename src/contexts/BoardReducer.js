a2bee6e0bcba29ca43c9ca018fc0bd51dfaad5c2
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-05-01 22:30:40 -0400

    add UPDATE_BOX_COLOR case and update initialState

--- src/contexts/BoardReducer.js
+++ src/contexts/BoardReducer.js
@@ -1,43 +1,53 @@
-import WORDS from "../assets/words"
+import getIdioms from "../assets/words"
 
 const NUMBER_OF_GUESSES = 6
-const randomWord = WORDS[Math.floor(Math.random() * WORDS.length)]
+const idioms = getIdioms()
+const randomWord = idioms[Math.floor(Math.random() * idioms.length)]
 
-const initialState = {
-  board: new Array(NUMBER_OF_GUESSES)
-    .fill(0)
-    .map(() => new Array(randomWord.length).fill("")),
+export const initialState = {
+  board: new Array(NUMBER_OF_GUESSES).fill(0).map((outerEl, rowIdx) => {
+    const rowArray = new Array(randomWord.length)
+      .fill(0)
+      .map((innerEl, colIdx) => ({
+        character: "",
+        id: `${rowIdx}.${colIdx}`,
+        backgroundColor: "",
+      }))
+    rowArray.id = rowIdx
+    return rowArray
+  }),
   nextLetter: 0,
   rightGuessString: randomWord,
   guessesRemaining: NUMBER_OF_GUESSES,
   numberOfGuesses: NUMBER_OF_GUESSES,
 }
 
-const boardReducer = (state, action) => {
-  const { numberOfGuesses, guessesRemaining, nextLetter } = state
+export const boardReducer = (state, action) => {
+  const { numberOfGuesses, guessesRemaining, nextLetter, rightGuessString } =
+    state
   const boardCopy = state.board.slice()
   switch (action.type) {
     case "RESET_BOARD":
       return { initialState }
-    case "GAME_OVER":
+    case "WINNER":
       return {
         ...state,
         guessesRemaining: 0,
       }
     case "INSERT_LETTER":
-      boardCopy[numberOfGuesses - guessesRemaining][nextLetter] =
+      boardCopy[numberOfGuesses - guessesRemaining][nextLetter].character =
         action.character
       return {
         ...state,
         board: boardCopy,
-        nextLetter: nextLetter + 1,
+        nextLetter: Math.min(nextLetter + 1, rightGuessString.length - 1),
       }
     case "DELETE_LETTER":
-      boardCopy[numberOfGuesses - guessesRemaining][nextLetter - 1] = ""
+      boardCopy[numberOfGuesses - guessesRemaining][nextLetter].character = ""
       return {
         ...state,
         board: boardCopy,
-        nextLetter: nextLetter - 1,
+        nextLetter: Math.max(nextLetter - 1, 0),
       }
     case "SUBMIT_WORD":
       return {
@@ -45,9 +55,13 @@ const boardReducer = (state, action) => {
         nextLetter: 0,
         guessesRemaining: guessesRemaining - 1,
       }
+    case "UPDATE_BOX_COLOR":
+      boardCopy[action.row][action.col].backgroundColor = action.backgroundColor
+      return {
+        ...state,
+        board: boardCopy,
+      }
     default:
       return state
   }
 }
-
-export default { initialState, boardReducer }
